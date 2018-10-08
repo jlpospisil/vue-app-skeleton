36f1da34a96b18c2bc91133c38408fe94f8fff6e
Josh Pospisil <josh.pospisil@gmail.com>
2018-10-08 16:10:33 -0500

    Added HelloWorld component back in to provide example unit test

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -2,11 +2,16 @@ import { shallowMount } from '@vue/test-utils';
 import HelloWorld from '@/components/HelloWorld.vue';
 
 describe('HelloWorld.vue', () => {
+  const msg = 'new message';
+  const component = shallowMount(HelloWorld, {
+    propsData: { msg },
+  });
+
+  it('Is a vue component', () => {
+    expect(component.isVueInstance()).toBeTruthy();
+  });
+
   it('renders props.msg when passed', () => {
-    const msg = 'new message';
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg },
-    });
-    expect(wrapper.text()).toMatch(msg);
+    expect(component.text()).toMatch(msg);
   });
 });
